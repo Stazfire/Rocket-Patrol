e1a4f87b2ad64cb2a226a365e7a9285c32c56ab2
Victor <hkchung@ucsc.edu>
2020-04-13 14:16:57 -0700

    Player Spaceship added

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,6 +4,8 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // white rectangle borders
         this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
@@ -11,5 +13,21 @@ class Play extends Phaser.Scene {
         this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
         // green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+
+        // add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket');
     }
+
+    preload() {
+        // load images/tile sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    update() {
+        this.starfield.tilePositionX -= 8;
+    }
+
+    
 }
